36e8a896a4adb70dd1de4a23de276c672662cd34
mbsocasi <mbsocasi@espe.edu.ec>
2023-12-18 18:54:48 -0500

    first commit

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        
+        <h1>etiqueta para ejecutar veris</h1>
+        <a href="grupo05/index.php">VERIS</a>
+    </body>
+</html>
